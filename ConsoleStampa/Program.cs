b6b972d733ea45604e340e34a2954b5bf2f8b1ce
Alessio Minniti <91311582+minnitialessio@users.noreply.github.com>
2021-10-08 09:25:41 +0200

    Update Program.cs

--- ConsoleStampa/Program.cs
+++ ConsoleStampa/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleStampa
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Hello Ale!!1!11!!1!!!11!1");
+            Console.WriteLine("Hi");
         }
     }
 }
